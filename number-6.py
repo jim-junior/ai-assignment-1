50a38da84f58299bcf06c7a7b20d73d72360bd0c
jim-junior <jimjunior854@gmail.com>
2023-10-12 22:44:47 +0300

    Fix expaanded error

--- number-6.py
+++ number-6.py
@@ -1,15 +1,6 @@
 from collections import deque
 import heapq
 
-# Weighted graph representation
-weighted_graph = {
-    'S': {'A': 3, 'B': 1},
-    'A': {'S': 3, 'B': 2, 'C': 2},
-    'B': {'B': 1, 'A': 2, 'C': 3},
-    'C': {'A': 2, 'B': 3, 'D': 4, 'G': 4},
-    'D': {'C': 4, 'G': 1},
-    'G': {'C': 4, 'D': 1}
-}
 
 # Heuristics for each node
 heuristics = {
@@ -20,6 +11,38 @@ heuristics = {
     'D': 1,
     'G': 0
 }
+
+
+# Weighted graph representation
+weighted_graph = {
+    'S': {'A': 3, 'B': 1},
+    'A': {'S': 3, 'B': 2, 'C': 2},
+    'B': {'B': 1, 'A': 2, 'C': 3},
+    'C': {'A': 2, 'B': 3, 'D': 4, 'G': 4},
+    'D': {'C': 4, 'G': 1},
+    'G': {'C': 4, 'D': 1}
+}
+
+
+# Depth-First Search 
+def dfs(graph, start, goal):
+    reached = set()
+    stack = [(start, [start])]
+    expanded_states = []
+
+    while stack:
+        node, path = stack.pop()
+        expanded_states.append(node)
+        if node == goal:
+            return path, expanded_states, []  # Return an empty list for unexpanded states
+        if node not in reached:
+            reached.add(node)
+            neighbors = sorted(graph[node].keys())
+            for neighbor in reversed(neighbors):
+                if neighbor not in reached:
+                    stack.append((neighbor, path + [neighbor]))
+
+    return None, expanded_states, []  # Return an empty list for unexpanded states
 
 # Breadth-First Search (BFS)
 def bfs(graph, start, goal):
@@ -42,25 +65,6 @@ def bfs(graph, start, goal):
     return None, expanded_states, []  # Return an empty list for unexpanded states
 
 
-# Depth-First Search (DFS)
-def dfs(graph, start, goal):
-    visited = set()
-    stack = [(start, [start])]
-    expanded_states = []
-
-    while stack:
-        node, path = stack.pop()
-        expanded_states.append(node)
-        if node == goal:
-            return path, expanded_states, []  # Return an empty list for unexpanded states
-        if node not in visited:
-            visited.add(node)
-            neighbors = sorted(graph[node].keys())
-            for neighbor in reversed(neighbors):
-                if neighbor not in visited:
-                    stack.append((neighbor, path + [neighbor]))
-
-    return None, expanded_states, []  # Return an empty list for unexpanded states
 
 
 # Uniform Cost Search (UCS)
@@ -83,6 +87,27 @@ def ucs(graph, start, goal):
                     heapq.heappush(priority_queue, (new_cost, neighbor, path + [neighbor]))
 
     return None, expanded_states, []  # Return an empty list for unexpanded states
+
+# Greedy Search
+def greedy(graph, start, goal, heuristics):
+    visited = set()
+    priority_queue = [(heuristics[start], start, [start])]
+    expanded_states = []
+
+    while priority_queue:
+        _, node, path = heapq.heappop(priority_queue)
+        expanded_states.append(node)
+        if node == goal:
+            return path, expanded_states
+        if node not in visited:
+            visited.add(node)
+            neighbors = sorted(graph[node].keys())
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    heapq.heappush(priority_queue, (heuristics[neighbor], neighbor, path + [neighbor]))
+
+    unexpanded_states = [node for node in graph if node not in visited]
+    return None, expanded_states, unexpanded_states
 
 
 # A* Search
@@ -109,26 +134,6 @@ def astar(graph, start, goal, heuristics):
 
     return None, expanded_states
 
-# Greedy Search
-def greedy(graph, start, goal, heuristics):
-    visited = set()
-    priority_queue = [(heuristics[start], start, [start])]
-    expanded_states = []
-
-    while priority_queue:
-        _, node, path = heapq.heappop(priority_queue)
-        expanded_states.append(node)
-        if node == goal:
-            return path, expanded_states
-        if node not in visited:
-            visited.add(node)
-            neighbors = sorted(graph[node].keys())
-            for neighbor in neighbors:
-                if neighbor not in visited:
-                    heapq.heappush(priority_queue, (heuristics[neighbor], neighbor, path + [neighbor]))
-
-    unexpanded_states = [node for node in graph if node not in visited]
-    return None, expanded_states, unexpanded_states
 
 # Calculate path cost
 def path_cost(path):
@@ -147,6 +152,15 @@ ucs_result = ucs(weighted_graph, start_node, goal_node)
 astar_result = astar(weighted_graph, start_node, goal_node, heuristics)
 greedy_result = greedy(weighted_graph, start_node, goal_node, heuristics)
 
+# Depth-First Search
+dfs_path, dfs_expanded, dfs_unexpanded = dfs(weighted_graph, start_node, goal_node)
+if dfs_path is not None:
+    print("DFS Path:", dfs_path)
+else:
+    print("DFS did not find a path.")
+print("DFS Expanded States:", dfs_expanded)
+print("DFS Unexpanded States:", dfs_unexpanded)
+
 
 # Breadth-First Search
 bfs_path, bfs_expanded, bfs_unexpanded = bfs(weighted_graph, start_node, goal_node)
@@ -157,14 +171,7 @@ else:
 print("BFS Expanded States:", bfs_expanded)
 print("BFS Unexpanded States:", bfs_unexpanded)
 
-# Depth-First Search
-dfs_path, dfs_expanded, dfs_unexpanded = dfs(weighted_graph, start_node, goal_node)
-if dfs_path is not None:
-    print("DFS Path:", dfs_path)
-else:
-    print("DFS did not find a path.")
-print("DFS Expanded States:", dfs_expanded)
-print("DFS Unexpanded States:", dfs_unexpanded)
+
 
 # Uniform Cost Search
 ucs_path, ucs_expanded, ucs_unexpanded = ucs(weighted_graph, start_node, goal_node)
@@ -175,10 +182,21 @@ else:
 print("UCS Expanded States:", ucs_expanded)
 print("UCS Unexpanded States:", ucs_unexpanded)
 
+greedy_path, greedy_expanded = greedy(weighted_graph, start_node, goal_node, heuristics)
+
+
+
+# Greedy
+if greedy_path is not None:
+    print("Greedy Search Path:", greedy_path)
+    print("Greedy Search Expanded States:", greedy_expanded)
+else:
+    print("Greedy Search did not find a path.")
+
 
 astar_path, astar_expanded = astar(weighted_graph, start_node, goal_node, heuristics)
 
-# Print A* Search results
+# A* 
 if astar_path is not None:
     print("A* Search Path:", astar_path)
     print("A* Search Expanded States:", astar_expanded)
@@ -187,14 +205,8 @@ else:
 
 
 
-greedy_path, greedy_expanded = greedy(weighted_graph, start_node, goal_node, heuristics)
 
-# Print Greedy Search results
-if greedy_path is not None:
-    print("Greedy Search Path:", greedy_path)
-    print("Greedy Search Expanded States:", greedy_expanded)
-else:
-    print("Greedy Search did not find a path.")
+
 
 
 
